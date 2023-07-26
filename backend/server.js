5cd2604e2c8abbb2dad5abb338440b93b24b3168
migue <miguelanjo2@hotmail.com>
2023-07-26 15:31:53 +0100

    create and delete workout query

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ try {
   const getWorkout = await pool.query('SELECT * FROM workouts WHERE Id = $1',[id]);
   res.json(getWorkout.rows)
  
-} catch (error) {
+} catch (err) {
   console.log(err);
   
 }
@@ -38,13 +38,33 @@ try {
 })
  
  //POST a new workout
- app.post('/', (req, res) => {
-  res.json({mssg: 'POST a new workout'})
+ app.post('/workouts', async (req, res) => {
+  const {title, loads, reps} = req.body;
+  console.log(title, loads, reps);
+
+  try {
+    const createWorkout = await pool.query(
+      `INSERT INTO workouts (Tiltle, Loads, Reps) VALUES($1, $2, $3)`,[title, loads, reps]);
+    res.json(createWorkout);
+  } catch (err) {
+    console.log(err);
+  }
+
+  
  })
  
  //DELETE  a workout
- app.delete('/:id', (req, res) => {
-  res.json({mssg: 'DELETE  a workout'})
+ app.delete('/workout/:id', async(req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteWorkout = await pool.query('DELETE FROM workouts WHERE Id = $1;', [
+      id,
+    ]);
+    res.json(deleteWorkout);
+  } catch (err) {
+    console.error(err);
+  }
+
  })
  
  
