229d77e173eb425fd4bef1ada4c26dbce0c80d55
migue <miguelanjo2@hotmail.com>
2023-08-13 12:31:50 +0100

    updated workout details

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -3,14 +3,14 @@ import React from 'react'
 const WorkoutDetails = ({workout}) => {
   return (
    <div className="workout-details">
-   <h4>{workout.title}</h4>
+   <h4>{workout.Tiltle}</h4>
    <p>
      <strong>Load (kg): </strong>
-     {workout.loads}
+     {workout.Loads}
    </p>
    <p>
      <strong>Reps: </strong>
-     {workout.reps}
+     {workout.Reps}
    </p>
    
  </div>
