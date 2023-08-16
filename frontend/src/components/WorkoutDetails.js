6b2e93e750cbad7ec95f9095ce5d8829006a7b09
migue <miguelanjo2@hotmail.com>
2023-08-16 12:12:04 +0100

    added handelClick function to sen DELETE request

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const WorkoutDetails = ({workout}) => {
+const WorkoutDetails = ({workout, onDelete}) => {
+
+  const handleClick = async() =>{
+    const response = await fetch('/workout/' + workout.id,{
+      method: 'DELETE'
+    })
+
+    if (response.ok){
+      onDelete(workout.id);
+
+    } else {
+      console.error('Failed to delete workout')
+    }
+
+  }
+  
+  
   return (
    <div className="workout-details">
-   <h4>{workout.title}</h4>
+   <h4>{workout.tiltle}</h4>
    <p>
      <strong>Load (kg): </strong>
      {workout.loads}
@@ -12,6 +28,7 @@ const WorkoutDetails = ({workout}) => {
      <strong>Reps: </strong>
      {workout.reps}
    </p>
+   <span onClick={handleClick}>DELETE </span>
    
  </div>
   )
