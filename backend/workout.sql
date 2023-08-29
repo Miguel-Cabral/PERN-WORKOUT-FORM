5069dc5292bad338a6315e2af52e7611477f221b
migue <miguelanjo2@hotmail.com>
2023-08-29 11:59:31 +0100

    updated table workouts

--- backend/workout.sql
+++ backend/workout.sql
@@ -8,3 +8,14 @@ CREATE TABLE workouts (
 INSERT INTO workouts (Tiltle, Loads, Reps) VALUES ('Biceps', 12, 3);
 INSERT INTO workouts (Tiltle, Loads, Reps) VALUES ('Triceps', 3, 12);
 INSERT INTO workouts (Tiltle, Loads, Reps) VALUES ('Push up', 4, 15);
+INSERT INTO workouts (Tiltle, Loads, Reps, WorkoutDate, Series)
+VALUES ('Legs', 100, 10, CURRENT_TIMESTAMP, 3);
+
+ALTER TABLE workouts
+ADD COLUMN series INTEGER;
+
+ALTER TABLE workouts
+DROP COLUMN workoutDate;
+
+ALTER TABLE workouts
+ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
